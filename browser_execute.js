682cdaa6aaacff2f1cd3e38559d0444a8b46f8f4
DeLuxe1337001 <74394136+DeLuxe1337001@users.noreply.github.com>
2021-08-07 22:01:48 -0400

    Fixed the letters innertext grab

--- browser_execute.js
+++ browser_execute.js
@@ -8,7 +8,7 @@ Wordsocket.onmessage = function (event) {
 
 setInterval(function(){ 
     if(document.querySelector("body > div.main.page > div.bottom > div.round > div.otherTurn > span.player").innerText == Username) {
-        var Letters = document.querySelector("body > div.main.page > div.bottom > div.round > div.otherTurn > span.player").innerText;
+        var Letters = document.querySelector("body > div.main.page > div.middle > div.canvasArea > div.round > div").innerText;
         Wordsocket.send(Letters);
     } 
 }, 1000);
